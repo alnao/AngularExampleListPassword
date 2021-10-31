8c3d31b8adf64603f6d5cb05d07b195175953b57
Alberto Nao <alnao@alnao.it>
2021-10-31 11:54:07 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ElencoPasswordComponent } from './elenco-password/elenco-password.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ElencoPasswordComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
